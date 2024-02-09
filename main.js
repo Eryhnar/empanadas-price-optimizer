4a31244e984bf6db3ef0b92a56373f5d33c0e8d9
pfernandez <pedrofernbel@gmail.com>
2024-02-09 16:43:20 +0100

    Testing answer1

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
-export const fn = (/* argumentos de la función */) => {
-   // Cuerpo de la función: Completa la lógica de la función aquí.
+export const fn = (a, b, c) => {
+   
+   // let total = 0;
+   // let highest = "";
+   // let lowest = "";
+
+   // if (a+b+c < 40 && a+b+c % 3 === 0) {
+   //    while (a+b+c > 0) {
+   //       if (c > 0) {
+   //          a
+   //       }
+         
+   //    }
+   // } else {
+   //    console.log('input error');
+   // }
+
+   console.log(a+b+c);
+   return Math.ceil((a+b+c)/3);
 };
